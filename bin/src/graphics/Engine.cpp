56168372ef829f4f8c8944410dba8a8d0e02d41c
glup21 <62499991+glup21@users.noreply.github.com>
2025-11-11 12:59:58 +0100

    Fixed skybox

--- bin/src/graphics/Engine.cpp
+++ bin/src/graphics/Engine.cpp
@@ -33,15 +33,11 @@ void Engine::Run()
         //glClearColor(0.12f, 0.09f, 0.18f, 1.0f); 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        glDepthFunc(GL_LEQUAL);
         glDepthMask(GL_FALSE);
         appContext->scene->GetCamera()->RenderSkybox();
         glDepthMask(GL_TRUE);
-
-        // // Temporary to fix light counting, need to introduce a better way later
-        // for (auto& light : *lights)
-        // {
-        //     light->Update();
-        // }
+        glDepthFunc(GL_LESS); 
 
         for (auto& gameObject : *gameObjects)
         {
